035b932dbcb0d67b422958b7eccd181bda50c52e
Ahmad Fatayri <ahmadfatayri@Houssams-MacBook-Pro.local>
2019-03-28 15:33:41 +0200

    init project

---
+++ rxnamer/rxnamer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  rxnamer
+//
+//  Created by Ahmad Fatayri on 3/28/19.
+//  Copyright © 2019 Ahmad Fatayri. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
