6782ab0fca9a346cd960b529e916a3647450f248
Ahmad Fatayri <ahmadfatayri@Houssams-MacBook-Pro.local>
2019-03-28 16:41:48 +0200

    not finished

---
+++ rxnamer/rxnamer/AddNameVC.swift
@@ -0,0 +1,30 @@
+//
+//  AddNameVC.swift
+//  rxnamer
+//
+//  Created by Ahmad Fatayri on 3/28/19.
+//  Copyright © 2019 Ahmad Fatayri. All rights reserved.
+//
+
+import UIKit
+
+class AddNameVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- rxnamer/rxnamer/ViewController.swift
+++ rxnamer/rxnamer/ViewController.swift
@@ -7,14 +7,53 @@
 //
 
 import UIKit
+import RxCocoa
+import RxSwift
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var nameEntryTextField: UITextField!
+    @IBOutlet weak var submitBtn: UIButton!
+    @IBOutlet weak var namesLbl: UILabel!
+    @IBOutlet weak var helloLbl: UILabel!
+    
+    let disposeBag = DisposeBag()
+    var namesArray: Variable<[String]> = Variable([])
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        bindTextField()
+        bindSubmitButton()
     }
 
-
+    func bindTextField() {
+        
+        nameEntryTextField.rx.text
+            .debounce(0.5, scheduler: MainScheduler.instance)
+            .map {
+                if $0 == "" {
+                    return "Type your name below"
+                }
+                else {
+                    return "Hello, \($0!)."
+                }
+            }
+            .bind(to: helloLbl.rx.text)
+            .addDisposableTo(disposeBag)
+    }
+    
+    func bindSubmitButton() {
+        submitBtn.rx.tap
+            .subscribe(onNext: {
+                if self.nameEntryTextField.text != "" {
+                    self.namesArray.value.append(self.nameEntryTextField.text!)
+                    self.namesLbl.rx.text.onNext(self.namesArray.value.joined(separator: ", "))
+                    self.nameEntryTextField.rx.text.onNext("")
+                    self.helloLbl.rx.text.onNext("Type your name below")
+                }
+            })
+            .addDisposableTo(disposeBag)
+    }
+    
 }
 
